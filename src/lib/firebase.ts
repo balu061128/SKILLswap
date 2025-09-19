ba12a45a607d5c59b4fa96d7d938e13c2827aa3d
balu perla <baluperla66@gmail.com>
2025-09-19 21:21:30 +0000

    connect this with my firebase app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBmlUFZBGZIbC0J0gMRpRyZKgKkCYVqMiU",
   authDomain: "studio-4614444882-36f96.firebaseapp.com",
   projectId: "studio-4614444882-36f96",
-  storageBucket: "studio-4614444882-36f96.appspot.com",
+  storageBucket: "studio-4614444882-36f96.firebasestorage.app",
   messagingSenderId: "264400307466",
   appId: "1:264400307466:web:4b60e52424a98e4826e6f3"
 };
